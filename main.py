8bd2e52628aecab14c9c044a8c72cf2d768d4775
tyferly <tyferly2003@gmail.com>
2024-01-11 17:36:44 +0400

    настройка CORS

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from functions.base64ToImage import base64ToImage
 from models.ImageBase64 import ImageBase64
@@ -6,6 +7,13 @@ from networks.models.numbers.networkNumbers import predictNumbers
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def readRoot():
